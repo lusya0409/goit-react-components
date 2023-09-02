ed652eb849e9de5c00f1193e65054d65328c6048
lusya0409 <lusya0409@gmail.com>
2023-09-03 02:23:15 +0300

    add api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,16 @@
 import { QuizForm } from './QuizForm/QuizForm';
 import { QuizList } from './QuizList/QuizList';
 import { SearchBar } from './SearchBar/SearchBar';
-import initialQuizItems from '../quiz-items.json';
+// import initialQuizItems from '../quiz-items.json'; заменили на бекенд
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout';
 import { Counter } from './Counter/Counter';
 import { Dropdown } from './Dropdown/Dropdown';
 import { ColorPicker } from './ColorPicker/ColorPicker';
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
+import { fetchQuizzes, deleteQuizById, createQuiz } from './api';
+import toast, { Toaster } from 'react-hot-toast';
 
 const colorPickerOptions = [
   { label: 'red', color: '#F44336' },
@@ -29,13 +31,16 @@ export class App extends Component {
 
   // вложенное состояние
   state = {
-    quizItems: initialQuizItems,
+    // quizItems: initialQuizItems, //заменили на бекенд стал пустой масив
+    quizItems: [],
+    loading: false,
+    error: false,
     filters: {
       topic: '',
       level: 'all',
     },
   };
-  componentDidMount() {
+  async componentDidMount() {
     const savedFilters = localStorage.getItem('quiz-filters');
     if (savedFilters !== null) {
       // const filters = JSON.parse(savedFilters);
@@ -47,30 +52,56 @@ export class App extends Component {
         filters: JSON.parse(savedFilters),
       });
     }
+    try {
+      this.setState({ loading: true });
+      const quizzes = await fetchQuizzes();
+      this.setState({ quizItems: quizzes });
+    } catch (error) {
+      this.setState({ error: true });
+    } finally {
+      this.setState({ loading: false });
+    }
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log(`prevState`, prevState);
-    console.log(`this.state`, this.state);
+  async componentDidUpdate(prevProps, prevState) {
     if (prevState.filters !== this.state.filters)
       localStorage.setItem('quiz-filters', JSON.stringify(this.state.filters));
   }
-  addQuiz = newQuiz => {
-    this.setState(prevState => ({
-      quizItems: [
-        ...prevState.quizItems,
-        {
-          id: nanoid(),
-          ...newQuiz,
-        },
-      ],
-    }));
+  addQuiz = async newQuiz => {
+    try {
+      this.setState({ loading: true, error: false });
+      const addedQuiz = await createQuiz({ newQuiz });
+
+      this.setState(prevState => ({
+        quizItems: [...prevState.quizItems, addedQuiz],
+      }));
+    } catch (error) {
+      this.setState({ error: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+
     console.log(newQuiz);
   };
-  deleteQuiz = quizId => {
-    this.setState(prevState => ({
-      quizItems: prevState.quizItems.filter(quiz => quiz.id !== quizId),
-    }));
+  deleteQuiz = async quizId => {
+    try {
+      this.setState({
+        loading: true,
+        error: false,
+      });
+
+      const deleteQuiz = await deleteQuizById(quizId);
+      this.setState(prevState => ({
+        quizItems: prevState.quizItems.filter(
+          quiz => quiz.id !== deleteQuiz.id
+        ),
+      }));
+      toast.success('Все очень хорошо!');
+    } catch (error) {
+      this.setState({ error: true });
+    } finally {
+      this.setState({ loading: false });
+    }
   };
   // changeLevelFilter = newLevel => {
   //   this.setState({
@@ -128,7 +159,7 @@ export class App extends Component {
     });
   };
   render() {
-    const { filters } = this.state;
+    const { filters, loading, error } = this.state;
     //вычисляемое свойство
     const visibleItems = this.getVisibleQuizItems();
 
@@ -144,10 +175,15 @@ export class App extends Component {
           onChangeTopic={this.changeTopicFilter}
           onReset={this.resetFilters}
         />
-        <QuizList items={visibleItems} onDelete={this.deleteQuiz} />
+        {loading && <div>LOADING...</div>}
+        {error && !loading && <div>OOPS! THERE WAS AN ERROR!</div>}
+        {visibleItems.length > 0 && (
+          <QuizList items={visibleItems} onDelete={this.deleteQuiz} />
+        )}
         <Counter />
 
         <GlobalStyle />
+        <Toaster />
       </Layout>
     );
   }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -29,6 +29,8 @@ export const SearchBar = ({
         <option value="intermediate">Intermediate</option>
         <option value="advanced">Advanced</option>
       </select>
+      {/* пока лоадинг кнопку прятать, изначально тоже прятать, пока у нас пустой
+      массив */}
       <button onClick={onReset}>Reset filters</button>
     </div>
   );
